76efcfb2c11b28db9f9ccca267179ab36da07a91
Michael O'Connor <B>
2021-06-22 19:41:50 +1200

    Intial Commit
    Initial seeds dataset from deepxplore
    
    #resolves #3

---
+++ .ipynb_checkpoints/DL-DIFF-CHECKER-checkpoint.py
@@ -0,0 +1,38 @@
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg19 import VGG19
+from keras.layers import Input
+import argparse
+
+# read the parameter
+# argument parsing
+parser = argparse.ArgumentParser( description='Main function for difference-inducing input generation in ImageNet dataset')
+parser.add_argument('seeds', help="number of seeds of input", type=int)
+
+args = parser.parse_args()
+
+
+# input image dimensions
+img_rows, img_cols = 224, 224
+input_shape = (img_rows, img_cols, 3)
+
+# define input tensor as a placeholder
+input_tensor = Input(shape=input_shape)
+
+# load multiple models sharing same input tensor
+K.set_learning_phase(0)
+model1 = VGG16(input_tensor=input_tensor)
+model2 = VGG19(input_tensor=input_tensor)
+
+# start gen inputs
+img_paths = image.list_pictures('./seeds/', ext='jpeg')
+
+for _ in xrange(args.seeds):
+    gen_img = preprocess_image(random.choice(img_paths))
+    orig_img = gen_img.copy()
+    # first check if input already induces differences
+    pred1, pred2, pred3 = model1.predict(gen_img), model2.predict(gen_img), model3.predict(gen_img)
+    label1, label2, label3 = np.argmax(pred1[0]), np.argmax(pred2[0]), np.argmax(pred3[0])
+
+    #This one here where we check if there is a difference
+    if not label1 == label2 == label3:
+        print("Difference found")
